a52087b1d0fa002b36b407deb93ec15ca39d7ea4
paulobernardo <paulo.bernardo@ilhasoft.com.br>
2021-06-17 11:29:39 -0300

    fix: jest configurations

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
+const path = require('path');
+
 module.exports = {
+  rootDir: path.resolve(__dirname),
   preset: '@vue/cli-plugin-unit-jest',
   collectCoverage: true,
   coverageReporters: ['lcov', 'text-summary'],
@@ -9,5 +12,11 @@ module.exports = {
     '!src/router.js',
     '!src/App.vue',
     '!**/node_modules/**',
+    '!tests/**',
   ],
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '.*\\.(vue)$': 'vue-jest',
+  },
+  moduleNameMapper: { '\\.(css|less)$': '<rootDir>/tests/__mocks__/styleMock.js' },
 };
