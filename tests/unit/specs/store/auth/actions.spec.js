93b6d5906be360a87eff8cbeeff0b7a54247a9a4
paulobernardo <paulo.bernardo@ilhasoft.com.br>
2021-06-30 16:14:01 -0300

    feat: store auth actions tests

---
+++ tests/unit/specs/store/auth/actions.spec.js
@@ -0,0 +1,59 @@
+import actions from '../../../../../src/store/auth/actions';
+
+describe('store/auth/actions.js', () => {
+  let context = {};
+  let state = {};
+  beforeEach(() => {
+    context = {
+      commit: jest.fn(),
+    };
+    state = {
+      token: 123,
+      org: 'org1',
+      project: 'bacf838a-b7c0-4cb1-9378-88ef972cdfec',
+    };
+  });
+
+  describe('MISSING', () => {
+    it('should NOT login if token is missing', async () => {
+      state.token = '';
+      await actions.externalLogin(context, state);
+
+      expect(context.commit).not.toHaveBeenCalled();
+    });
+
+    it('should NOT set org if org is missing', async () => {
+      state.org = '';
+      await actions.selectedOrg(context, state);
+
+      expect(context.commit).not.toHaveBeenCalled();
+    });
+
+    it('should NOT set project if project is missing', async () => {
+      state.project = null;
+      await actions.selectedProject(context, state);
+
+      expect(context.commit).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('PROVIDED', () => {
+    it('should login if token provided', async () => {
+      await actions.externalLogin(context, state);
+
+      expect(context.commit).toHaveBeenCalledTimes(1);
+    });
+
+    it('should set org if org is provided', async () => {
+      await actions.selectedOrg(context, state);
+
+      expect(context.commit).toHaveBeenCalledTimes(1);
+    });
+
+    it('should set project if project provided', async () => {
+      await actions.selectedProject(context, state);
+
+      expect(context.commit).toHaveBeenCalledTimes(1);
+    });
+  });
+});
