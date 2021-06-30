0f864aaa492915304e548e597a59f04d0f59e4e8
paulobernardo <paulo.bernardo@ilhasoft.com.br>
2021-06-30 15:52:08 -0300

    fix: store auth mutations tests

--- tests/unit/specs/store/auth/mutations.spec.js
+++ tests/unit/specs/store/auth/mutations.spec.js
@@ -2,11 +2,18 @@ import mutations from '../../../../../src/store/auth/mutations';
 import TYPES from '../../../../../src/store/types';
 
 describe('mutations', () => {
+  let state = {};
+
+  beforeEach(() => {
+    state = {
+      token: null,
+      org: null,
+      project: null,
+    };
+  });
+
   it('should set token to state.token', () => {
     const token = 123;
-    const state = {
-      token: null,
-    };
 
     mutations[TYPES.SET_TOKEN](state, token);
     expect(state.token).toEqual(token);
@@ -14,9 +21,6 @@ describe('mutations', () => {
 
   it('should set org to state.org', () => {
     const org = 'organization1';
-    const state = {
-      org: null,
-    };
 
     mutations[TYPES.SET_ORG](state, org);
     expect(state.org).toEqual(org);
@@ -24,11 +28,8 @@ describe('mutations', () => {
 
   it('should set project to state.project', () => {
     const project = 'bacf838a-b7c0-4cb1-9378-88ef972cdfec';
-    const state = {
-      project: null,
-    };
 
     mutations[TYPES.SET_PROJECT](state, project);
-    expect(state.org).toEqual(project);
+    expect(state.project).toEqual(project);
   });
 });
