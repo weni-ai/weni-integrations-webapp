b40b41877a81289b9b429938c5f8c36c1e07164b
paulobernardo <paulo.bernardo@weni.ai>
2021-12-21 13:38:44 -0300

    feat: send project uuid as header

--- src/api/request.js
+++ src/api/request.js
@@ -6,7 +6,12 @@ export default {
     const client = axios.create({
       baseURL: process.env.VUE_APP_API_BASE_URL,
       headers: {
-        ...(store.getters.authenticated ? { Authorization: `${store.getters.authToken}` } : {}),
+        ...(store.getters.authenticated
+          ? {
+              Authorization: `${store.getters.authToken}`,
+              'Project-Uuid': `${store.getters.getSelectedProject}`,
+            }
+          : {}),
       },
     });
 
