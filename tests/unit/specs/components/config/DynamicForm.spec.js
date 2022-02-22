cc563269d497ca759825c6364ce2f76fd7e71713
paulobernardo <paulo.bernardo@weni.ai>
2022-02-22 12:37:43 -0300

    feat: tests

---
+++ tests/unit/specs/components/config/DynamicForm.spec.js
@@ -0,0 +1,94 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import DynamicForm from '@/components/config/DynamicForm.vue';
+import i18n from '@/utils/plugins/i18n';
+
+const localVue = createLocalVue();
+
+describe('DynamicForm.vue', () => {
+  let wrapper;
+  beforeEach(() => {
+    const inputs = [
+      {
+        type: 'input',
+        name: 'input',
+        label: 'input',
+        value: null,
+      },
+      {
+        type: 'select',
+        name: 'select',
+        label: 'select',
+        placeholder: 'select',
+        value: null,
+        options: [
+          { value: '1', text: 'select1' },
+          { value: '2', text: 'select2' },
+        ],
+      },
+    ];
+
+    wrapper = shallowMount(DynamicForm, {
+      localVue,
+      i18n,
+      propsData: {
+        inputs,
+      },
+      mocks: {
+        $t: () => 'some specific text',
+      },
+      stubs: {
+        UnnnicInput: true,
+        UnnnicSelect: true,
+      },
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be rendered properly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call emitInput on input component event', async () => {
+    const spy = spyOn(wrapper.vm, 'emitInput');
+    const input = wrapper.findComponent({ ref: 'unnnic-input' });
+
+    expect(spy).not.toHaveBeenCalled();
+    input.vm.$emit('input', 'test');
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(0, wrapper.props().inputs[0], 'test');
+  });
+
+  it('should call emitInput on select component event', async () => {
+    const spy = spyOn(wrapper.vm, 'emitInput');
+    const input = wrapper.findComponent({ ref: 'unnnic-select' });
+
+    expect(spy).not.toHaveBeenCalled();
+    input.vm.$emit('input', 2);
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(1, wrapper.props().inputs[1], 2);
+  });
+
+  describe('emitInput()', () => {
+    it('should emit input value if default type', () => {
+      const index = 0;
+      const input = wrapper.props().inputs[index];
+      const event = 'test';
+      expect(wrapper.emitted('input')).toBeFalsy();
+      wrapper.vm.emitInput(index, input, event);
+      expect(wrapper.emitted('input')).toBeTruthy();
+      expect(wrapper.emitted('input')[0]).toEqual([{ index, value: event }]);
+    });
+    it('should emit option value if input type is select', () => {
+      const index = 1;
+      const input = wrapper.props().inputs[index];
+      const event = 1;
+      expect(wrapper.emitted('input')).toBeFalsy();
+      wrapper.vm.emitInput(index, input, event);
+      expect(wrapper.emitted('input')).toBeTruthy();
+      expect(wrapper.emitted('input')[0]).toEqual([{ index, value: input.options[event].value }]);
+    });
+  });
+});
---
+++ tests/unit/specs/components/config/channels/whatsapp/__snapshots__/Config.spec.js
@@ -0,0 +1,86 @@
+import { unnnicCallAlert as mockUnnnicCallAlert } from '@weni/unnnic-system';
+
+jest.mock('@weni/unnnic-system', () => ({
+  ...jest.requireActual('@weni/unnnic-system'),
+  unnnicCallAlert: jest.fn(),
+}));
+
+import Vuex from 'vuex';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import { singleApp } from '../../../../../../../__mocks__/appMock.js';
+import i18n from '@/utils/plugins/i18n';
+
+import whatsappConfig from '@/components/config/channels/whatsapp/Config.vue';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('TelegramConfig.vue', () => {
+  let wrapper;
+
+  let actions;
+  let store;
+
+  beforeEach(() => {
+    actions = {
+      updateAppConfig: jest.fn(),
+      getApp: jest.fn(() => {
+        return { data: singleApp };
+      }),
+    };
+
+    store = new Vuex.Store({
+      actions,
+    });
+
+    wrapper = shallowMount(whatsappConfig, {
+      localVue,
+      i18n,
+      store,
+      propsData: {
+        app: singleApp,
+      },
+      stubs: {
+        DynamicForm: true,
+      },
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be rendered properly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('updateContactInfoInput()', () => {
+    it('should set contact info input data correctly', async () => {
+      const inputData = {
+        index: 0,
+        value: 'value',
+      };
+      expect(wrapper.vm.contactInfoInputs[inputData.index].value).not.toEqual(inputData.value);
+      wrapper.vm.updateContactInfoInput(inputData);
+      expect(wrapper.vm.contactInfoInputs[inputData.index].value).toEqual(inputData.value);
+    });
+  });
+  describe('updateProfileInput()', () => {
+    it('should set profile info input data correctly', async () => {
+      const inputData = {
+        index: 0,
+        value: 'value',
+      };
+      expect(wrapper.vm.profileInputs[inputData.index].value).not.toEqual(inputData.value);
+      wrapper.vm.updateProfileInput(inputData);
+      expect(wrapper.vm.profileInputs[inputData.index].value).toEqual(inputData.value);
+    });
+  });
+  describe('closeConfig()', () => {
+    it('should emit closeModal', () => {
+      expect(wrapper.emitted('closeModal')).toBeFalsy();
+      wrapper.vm.closeConfig();
+      expect(wrapper.emitted('closeModal')).toBeTruthy();
+    });
+  });
+});
