2a28dfa54547034835c665c49f0462ccb684ad9c
paulobernardo <paulo.bernardo@weni.ai>
2022-08-04 16:21:46 -0300

    refactor myApps actions

--- src/store/myApps/actions.js
+++ src/store/myApps/actions.js
@@ -1,10 +1,22 @@
 import appType from '@/api/myApps';
 
 export default {
-  async getConfiguredApps(store, { params }) {
-    return await appType.getConfiguredApps(params);
+  async getConfiguredApps({ commit }, { params }) {
+    commit('GET_CONFIGURED_APPS_REQUEST');
+    try {
+      const { data } = await appType.getConfiguredApps(params);
+      commit('GET_CONFIGURED_APPS_SUCCESS', data);
+    } catch (err) {
+      commit('GET_CONFIGURED_APPS_ERROR', err);
+    }
   },
-  async getInstalledApps(store, { params }) {
-    return await appType.getInstalledApps(params);
+  async getInstalledApps({ commit }, { params }) {
+    commit('GET_INSTALLED_APPS_REQUEST');
+    try {
+      const { data } = await appType.getInstalledApps(params);
+      commit('GET_INSTALLED_APPS_SUCCESS', data);
+    } catch (err) {
+      commit('GET_INSTALLED_APPS_ERROR', err);
+    }
   },
 };
