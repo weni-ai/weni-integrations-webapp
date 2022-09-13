1d19a396d9dc8235a687f47979656087fdaf4332
Paulo Bernardo <paulo.bernardo@weni.ai>
2022-09-13 12:01:35 -0300

    feat: tests

--- tests/unit/specs/store/auth/actions.spec.js
+++ tests/unit/specs/store/auth/actions.spec.js
@@ -1,73 +1,124 @@
-import actions from '../../../../../src/store/auth/actions';
+jest.mock('@/api/auth', () => {
+  return {
+    getFlowToken: jest.fn(),
+  };
+});
+
+import { createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import actions from '@/store/auth/actions';
+import state from '@/store/auth/state';
+import mutations from '@/store/auth/mutations';
+import authApi from '@/api/auth';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
 describe('store/auth/actions.js', () => {
-  let context = {};
-  let state = {};
+  let store;
+
   beforeEach(() => {
-    context = {
-      commit: jest.fn(),
-    };
-    state = {
-      token: 123,
-      org: 'org1',
-      project: 'bacf838a-b7c0-4cb1-9378-88ef972cdfec',
-      flowOrg: '88ef972c-9378-4cb1-b7c0-bacf838adfec',
-    };
+    store = new Vuex.Store({
+      modules: {
+        auth: {
+          state,
+          actions,
+          mutations,
+        },
+      },
+    });
   });
 
   describe('MISSING', () => {
     it('should NOT login if token is missing', async () => {
-      state.token = '';
-      await actions.externalLogin(context, state);
-
-      expect(context.commit).not.toHaveBeenCalled();
+      store.state.auth.token = null;
+      await store.dispatch('externalLogin', '');
+      expect(store.state.auth.token).toEqual(null);
     });
 
     it('should NOT set org if org is missing', async () => {
-      state.org = '';
-      await actions.selectedOrg(context, state);
-
-      expect(context.commit).not.toHaveBeenCalled();
+      store.state.auth.org = null;
+      await store.dispatch('selectedOrg', '');
+      expect(store.state.auth.org).toEqual(null);
     });
 
     it('should NOT set project if project is missing', async () => {
-      state.project = null;
-      await actions.selectedProject(context, state);
-
-      expect(context.commit).not.toHaveBeenCalled();
+      store.state.auth.project = null;
+      await store.dispatch('selectedProject', '');
+      expect(store.state.auth.project).toEqual(null);
     });
 
     it('should NOT set flowOrg if flowOrg is missing', async () => {
-      state.flowOrg = null;
-      await actions.selectedFlowOrg(context, state);
-
-      expect(context.commit).not.toHaveBeenCalled();
+      store.state.auth.flowOrg = null;
+      await store.dispatch('selectedFlowOrg', '');
+      expect(store.state.auth.flowOrg).toEqual(null);
     });
   });
 
   describe('PROVIDED', () => {
     it('should login if token provided', async () => {
-      await actions.externalLogin(context, state);
-
-      expect(context.commit).toHaveBeenCalledTimes(1);
+      store.state.auth.token = null;
+      await store.dispatch('externalLogin', { token: '123' });
+      expect(store.state.auth.token).toEqual('123');
     });
 
     it('should set org if org is provided', async () => {
-      await actions.selectedOrg(context, state);
-
-      expect(context.commit).toHaveBeenCalledTimes(1);
+      store.state.auth.org = null;
+      await store.dispatch('selectedOrg', { org: 'org' });
+      expect(store.state.auth.org).toEqual('org');
     });
 
     it('should set project if project provided', async () => {
-      await actions.selectedProject(context, state);
-
-      expect(context.commit).toHaveBeenCalledTimes(1);
+      store.state.auth.project = null;
+      await store.dispatch('selectedProject', { project: 'project' });
+      expect(store.state.auth.project).toEqual('project');
     });
 
     it('should set flowOrg if flowOrg provided', async () => {
-      await actions.selectedFlowOrg(context, state);
+      store.state.auth.flowOrg = null;
+      await store.dispatch('selectedFlowOrg', { flowOrg: 'org123' });
+      expect(store.state.auth.flowOrg).toEqual('org123');
+    });
+  });
 
-      expect(context.commit).toHaveBeenCalledTimes(1);
+  describe('getFlowToken()', () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+
+      authApi.getFlowToken.mockImplementation(() => {
+        return Promise.resolve({ data: { api_token: '123' } });
+      });
+    });
+
+    it('should call getFlowToken from API', async () => {
+      expect(authApi.getFlowToken).not.toHaveBeenCalled();
+      await store.dispatch('getFlowToken');
+      expect(authApi.getFlowToken).toHaveBeenCalledTimes(1);
+    });
+
+    it('should set returned app as result data', async () => {
+      store.state.auth.flowToken = null;
+      expect(store.state.auth.flowToken).not.toEqual('123');
+      await store.dispatch('getFlowToken');
+      expect(store.state.auth.flowToken).toEqual('123');
+    });
+
+    it('should set loadingFlowToken to false', async () => {
+      store.state.auth.loadingFlowToken = true;
+      expect(store.state.auth.loadingFlowToken).toBe(true);
+      await store.dispatch('getFlowToken');
+      expect(store.state.auth.loadingFlowToken).toBe(false);
+    });
+
+    it('should set errorFlowToken as result data', async () => {
+      const error = { error: 'failed' };
+      authApi.getFlowToken.mockImplementation(() => {
+        return Promise.reject(error);
+      });
+      store.state.auth.errorFlowToken = {};
+      expect(store.state.auth.errorFlowToken).not.toEqual(error);
+      await store.dispatch('getFlowToken');
+      expect(store.state.auth.errorFlowToken).toEqual(error);
     });
   });
 });
--- tests/unit/specs/views/App.spec.js
+++ tests/unit/specs/views/App.spec.js
@@ -1,3 +1,10 @@
+jest.mock('@/api/auth', () => {
+  return {
+    getFlowToken: jest.fn(),
+  };
+});
+
+import Vuex from 'vuex';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import App from '@/App.vue';
 import i18n from '@/utils/plugins/i18n';
@@ -6,13 +13,25 @@ const router = new VueRouter();
 
 const localVue = createLocalVue();
 localVue.use(VueRouter);
+localVue.use(Vuex);
 
 describe('App.vue', () => {
   let wrapper;
+  let actions, store;
+
   beforeEach(() => {
+    actions = {
+      getFlowToken: jest.fn(),
+    };
+
+    store = new Vuex.Store({
+      actions,
+    });
+
     wrapper = shallowMount(App, {
       localVue,
       i18n,
+      store,
       router,
     });
   });
