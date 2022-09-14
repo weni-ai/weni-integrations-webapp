7983458e04b195eeca463ba9ad93ece10e6d0a9d
Paulo Bernardo <paulo.bernardo@weni.ai>
2022-09-14 15:27:45 -0300

    fix: get flow token url

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,6 +3,6 @@ import request from './request';
 const resource = '/api/v1';
 export default {
   getFlowToken() {
-    return request.$http.get(`${resource}/internal/user-api-token/`);
+    return request.$http.get(`${resource}/internal/user-api-token`);
   },
 };
