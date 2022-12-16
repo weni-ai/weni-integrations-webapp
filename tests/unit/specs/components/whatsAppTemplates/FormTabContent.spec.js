80d43c64977a046a1dc0c3d823ba43b8076a1222
Paulo Bernardo <paulo.bernardo@weni.ai>
2022-12-16 17:25:33 -0300

    feat: add tests

--- tests/unit/specs/components/whatsAppTemplates/FormTabContent.spec.js
+++ tests/unit/specs/components/whatsAppTemplates/FormTabContent.spec.js
@@ -31,6 +31,7 @@ const mountComponent = ({
   language = null,
   availableLanguages = [],
   templateResults = [],
+  canEdit = true,
 } = {}) => {
   if (!availableLanguages.length) {
     availableLanguages = [
@@ -94,6 +95,7 @@ const mountComponent = ({
       selectedForm,
       removeLanguages,
       availableLanguages,
+      canEdit,
     },
     stubs: {
       FormTabContentHeader: true,
@@ -133,7 +135,6 @@ describe('components/whatsAppTemplates/FormTabContent.vue', () => {
 
   it('should emit category change', async () => {
     const { wrapper, actions } = mountComponent();
-    console.log(wrapper.html());
     const categorySelectComponent = wrapper.findComponent(unnnicMultiSelect);
 
     expect(actions.updateTemplateForm).not.toHaveBeenCalled();
@@ -441,6 +442,17 @@ describe('components/whatsAppTemplates/FormTabContent.vue', () => {
       expect(wrapper.emitted('save-changes')).toBeFalsy();
 
       await saveButton.trigger('click');
+      await wrapper.vm.$nextTick();
+
+      expect(wrapper.emitted('save-changes')).toBeFalsy();
+    });
+
+    it('should not emit save-changes if cannot save', async () => {
+      const { wrapper } = mountComponent({ canEdit: false });
+
+      expect(wrapper.emitted('save-changes')).toBeFalsy();
+
+      await wrapper.vm.saveTemplate();
       await wrapper.vm.$nextTick();
 
       expect(wrapper.emitted('save-changes')).toBeFalsy();
