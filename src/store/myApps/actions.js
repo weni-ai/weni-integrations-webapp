2ebd7b4c6353677a28bfde8cfbb97b6d7ee4ff15
Paulo Bernardo <paulo.bernardo@weni.ai>
2022-12-23 00:16:47 -0300

    feat: update myApps actions to handle generic apps

--- src/store/myApps/actions.js
+++ src/store/myApps/actions.js
@@ -1,10 +1,24 @@
 import appType from '@/api/myApps';
+import { clearHtmlTags } from '@/utils/clearHtmlTags';
+
+/* istanbul ignore next */
+function formatGenericApp(appList) {
+  return appList.map((app) => {
+    const summary =
+      app.code === 'generic' ? clearHtmlTags(app.config.channel_claim_blurb) : app.summary;
+
+    return { ...app, generic: app.code === 'generic', summary };
+  });
+}
 
 export default {
   async getConfiguredApps({ commit }, { params }) {
     commit('GET_CONFIGURED_APPS_REQUEST');
     try {
-      const { data } = await appType.getConfiguredApps(params);
+      let { data } = await appType.getConfiguredApps(params);
+
+      data = formatGenericApp(data);
+
       commit('GET_CONFIGURED_APPS_SUCCESS', data);
     } catch (err) {
       commit('GET_CONFIGURED_APPS_ERROR', err);
@@ -13,7 +27,10 @@ export default {
   async getInstalledApps({ commit }, { params }) {
     commit('GET_INSTALLED_APPS_REQUEST');
     try {
-      const { data } = await appType.getInstalledApps(params);
+      let { data } = await appType.getInstalledApps(params);
+
+      data = formatGenericApp(data);
+
       commit('GET_INSTALLED_APPS_SUCCESS', data);
     } catch (err) {
       commit('GET_INSTALLED_APPS_ERROR', err);
