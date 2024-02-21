b577be55e9fab7befdc089031317efefd79d404b
acnormun <anaclaranoronha.m@gmail.com>
2024-02-21 12:48:13 -0300

    Feat: clean src, update node and install vue3 through vite

--- src/App.vue
+++ src/App.vue
@@ -1,144 +1,85 @@
-<template>
-  <div class="app">
-    <router-view class="content" />
-
-    <div class="survey-container">
-      <Survey />
-    </div>
-  </div>
-</template>
-
-<script>
-  import '@weni/unnnic-system';
-  import Survey from '@/components/Survey/index.vue';
-  import initHelpHero from 'helphero';
-  import getEnv from '@/utils/env';
-
-  export default {
-    name: 'App',
-    components: {
-      Survey,
-    },
-    data() {
-      return {
-        connectBaseURL: '',
-      };
-    },
-    mounted() {
-      if (getEnv('VUE_APP_HELPHERO_ID')) {
-        const hlp = initHelpHero(getEnv('VUE_APP_HELPHERO_ID'));
-        hlp.anonymous();
-      }
-
-      window.parent.postMessage(
-        {
-          event: 'getConnectBaseURL',
-        },
-        '*',
-      );
-      window.parent.postMessage(
-        {
-          event: 'getLanguage',
-        },
-        '*',
-      );
-      window.addEventListener('message', (event) => {
-        const eventName = event.data && event.data.event;
-        if (eventName === 'setConnectBaseURL') {
-          this.connectBaseURL = event.data.connectBaseURL;
-          this.translateAllLinks();
-        }
-        if (eventName === 'setLanguage') {
-          this.$i18n.locale = event.data.language;
-        }
-      });
-    },
-    methods: {
-      translateAllLinks() {
-        if (!this.connectBaseURL) {
-          return;
-        }
-        const url = new URL(this.connectBaseURL);
-        const debug = url.host && url.host.includes('develop');
-        document.querySelectorAll('a[href]').forEach((link) => {
-          const internalHref = link.getAttribute('internal-href') || link.getAttribute('href');
-          if (['http://', 'https://'].some((initial) => internalHref.startsWith(initial))) {
-            return;
-          }
-          const dashHref = this.connectBaseURL + internalHref;
-          if (link.translateLinkConnect) {
-            if (link.getAttribute('href') === dashHref) {
-              return;
-            }
-            link.removeEventListener('click', link.translateLinkConnect);
-          }
-          link.setAttribute('internal-href', internalHref);
-          link.setAttribute('href', dashHref);
-          const randomId = Math.floor(Math.random() * 100);
-          link.addEventListener(
-            'click',
-            (link.translateLinkConnect = () => {
-              if (debug) {
-                // eslint-disable-next-line no-console
-                console.log(`TranslateLinkConnectId ${randomId}`);
-              }
-              link.setAttribute('href', internalHref);
-              setTimeout(() => {
-                link.setAttribute('href', dashHref);
-              }, 0);
-            }),
-          );
-        });
-      },
-    },
-    updated() {
-      this.translateAllLinks();
-    },
-  };
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style lang="scss" scoped>
-  .app {
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
     display: flex;
-    flex-direction: column;
-    height: 100vh;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
 
-  .content {
-    margin: $unnnic-spacing-inset-md;
+  .logo {
+    margin: 0 2rem 0 0;
   }
 
-  .survey-container {
-    position: fixed;
-    bottom: 150px;
-    right: 18px;
-    z-index: 3;
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
   }
 
-  @supports (-moz-appearance: none) {
-    .survey-container {
-      right: 23px;
-    }
-  }
-</style>
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
 
-<style lang="scss">
-  html {
-    height: 100%;
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
-  body {
-    height: 100%;
-    margin: 0;
-    background-color: $unnnic-color-background-snow;
-    font-family: $unnnic-font-family-secondary, Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
-  }
-
-  .alert-container {
-    z-index: 9999;
-  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,46 +1,14 @@
-import Vue from 'vue';
-import * as Sentry from '@sentry/vue';
-import * as Integrations from '@sentry/integrations';
-import LogRocket from 'logrocket';
-import vClickOutside from 'v-click-outside';
-import App from './App.vue';
-import i18n from './utils/plugins/i18n';
-import router from './router';
-import store from './store';
-import getEnv from '@/utils/env';
-import { makeServer } from '@/miragejs/server';
+import './assets/main.css'
 
-Vue.use(vClickOutside);
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-Vue.config.productionTip = false;
+import App from './App.vue'
+import router from './router'
 
-if (getEnv('NODE_ENV') === 'development') {
-  makeServer();
-}
+const app = createApp(App)
 
-if (getEnv('VUE_APP_USE_SENTRY') && getEnv('VUE_APP_SENTRY_DSN')) {
-  Sentry.init({
-    dsn: getEnv('VUE_APP_SENTRY_DSN'),
-    integrations: [
-      new Integrations.Vue({
-        Vue,
-        attachProps: true,
-      }),
-    ],
-    logErrors: true,
-  });
-}
+app.use(createPinia())
+app.use(router)
 
-if (getEnv('VUE_APP_LOGROCKET_ID')) {
-  LogRocket.init(getEnv('VUE_APP_LOGROCKET_ID'), {
-    mergeIframes: true,
-    parentDomain: getEnv('VUE_APP_PARENT_IFRAME_DOMAIN'),
-  });
-}
-
-new Vue({
-  i18n,
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+app.mount('#app')
