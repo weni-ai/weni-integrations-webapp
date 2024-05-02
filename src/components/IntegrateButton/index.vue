d27e1042edc1fdec7625c7aea6da62e264cdd960
acnormun <anaclaranoronha.m@gmail.com>
2024-05-02 10:18:45 -0300

    fix: update integrate button

--- src/components/IntegrateButton/index.vue
+++ src/components/IntegrateButton/index.vue
@@ -21,8 +21,11 @@
 <script>
   import addModal from '../AddModal/index.vue';
   import configPopUp from '../config/ConfigPopUp.vue';
-  import { unnnicCallAlert } from '@weni/unnnic-system';
-  import { mapActions, mapState } from 'vuex';
+  // import unnnicCallAlert from '@weni/unnnic-system';
+  import alert from '@/utils/call';
+  import { mapActions, mapState } from 'pinia';
+  import { app_type } from '@/stores/modules/appType/appType.store';
+  import { auth_store } from '@/stores/modules/auth.store';
   import LoadingButton from '../LoadingButton/index.vue';
   import getEnv from '../../utils/env';
 
@@ -72,14 +75,11 @@
       window.openWACloudPopUp = this.openWACloudPopUp;
     },
     computed: {
-      ...mapState({
-        createAppResponse: (state) => state.appType.createAppResponse,
-        errorCreateApp: (state) => state.appType.errorCreateApp,
-        project: (state) => state.auth.project,
-      }),
+      ...mapState(app_type, ['createAppResponse', 'errorCreateApp']),
+      ...mapState(auth_store, ['project']),
     },
     methods: {
-      ...mapActions(['createApp']),
+      ...mapActions(app_type, ['createApp']),
       async addApp(app) {
         if (this.hasFBLoginList.includes(app.code) || app.config_design === 'pre-popup') {
           this.$refs.configPopUp.openPopUp(app);
@@ -118,7 +118,14 @@
         this.$refs.configPopUp.openPopUp(app, customData);
       },
       callErrorModal({ text }) {
-        unnnicCallAlert({
+        // unnnicCallAlert({
+        //   props: {
+        //     text,
+        //     type: 'error',
+        //   },
+        //   seconds: 6,
+        // });
+        alert.callAlert({
           props: {
             text,
             title: this.$t('general.error'),
