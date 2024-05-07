0faabdda0200700085315111039c7eb502ed27ff
acnormun <anaclaranoronha.m@gmail.com>
2024-05-07 18:23:25 -0300

    fiX: resolve merge conflicts

--- src/api/appType/externalServices/index.js
+++ src/api/appType/externalServices/index.js
@@ -3,19 +3,6 @@ import request from '@/api/request.js';
 const resource = '/api/v1/apptypes';
 
 export default {
-<<<<<<< HEAD
-  getAllExternalServicesTypes() {
-    return request.$http.get(`${resource}/?category=external`);
-  },
-  createPrompts(code, appUuid, payload) {
-    return request.$http.post(`${resource}/${code}/apps/${appUuid}/prompts/`, payload);
-  },
-  getPrompts(code, appUuid) {
-    return request.$http.get(`${resource}/${code}/apps/${appUuid}/prompts/`);
-  },
-  deletePrompts(code, appUuid, payload) {
-    return request.$http.delete(`${resource}/${code}/apps/${appUuid}/prompts/`, { data: payload });
-=======
   async getAllExternalServicesTypes() {
     return await request.$http.get(`${resource}/?category=external`).then((r) => r.data);
   },
@@ -35,6 +22,5 @@ export default {
         data: payload,
       })
       .then((r) => r.data);
->>>>>>> 2b3650211d3352b076cdf8f0ba61d30be889da5b
   },
 };
