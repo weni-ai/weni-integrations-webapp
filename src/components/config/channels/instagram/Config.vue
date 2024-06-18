56a76a8306a6aef7a784fb003098fd5fd77e1935
acnormun <anaclaranoronha.m@gmail.com>
2024-06-18 17:51:12 -0300

    fix: replace :value from instagram config

--- src/components/config/channels/instagram/Config.vue
+++ src/components/config/channels/instagram/Config.vue
@@ -16,13 +16,13 @@
           class="app-config-instagram__settings__content__inputs__name"
           type="normal"
           :disabled="true"
-          :value="app.config.page_name"
+          v-model="app.config.page_name"
         />
         <unnnic-input
           class="app-config-instagram__settings__content__inputs__id"
           type="normal"
           :disabled="true"
-          :value="`ID: ${app.config.page_id}`"
+          v-model="formattedPageId"
         />
       </div>
     </div>
@@ -38,6 +38,11 @@
         default: /* istanbul ignore next */ () => {},
       },
     },
+    computed: {
+    formattedPageId() {
+      return `ID: ${this.app.config.page_id}`;
+    }
+  }
   };
 </script>
 
