bb876d341f3455e5700f6b3a2fe46c0951ac8945
acnormun <anaclara.noronha@weni.ai>
2024-08-01 15:51:49 -0300

    feat: discovery tests

--- src/tests/views/Discovery.spec.js
+++ src/tests/views/Discovery.spec.js
@@ -2,17 +2,78 @@
 import { mount } from '@vue/test-utils';
 import { createTestingPinia } from '@pinia/testing';
 import Discovery from '../../views/Discovery/index.vue';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import i18n from '@/utils/plugins/i18n';
 
-describe('HelloWorld', () => {
+describe('Discovery', () => {
   it('matches snapshot', () => {
     const wrapper = mount(Discovery, {
       global: {
         plugins: [createTestingPinia(), i18n],
       },
-      props: { msg: 'Hello Vitest' },
     });
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  describe('mounted', () => {
+    it('should call fetchChannels', async () => {
+      const spy = vi.spyOn(Discovery.methods, 'fetchChannels');
+      const wrapper = mount(Discovery, {
+        global: {
+          plugins: [createTestingPinia({ createSpy: vi.fn }), i18n],
+        },
+      });
+      await wrapper.vm.$nextTick();
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call fetchExternalServices', async () => {
+      const spy = vi.spyOn(Discovery.methods, 'fetchExternalServices');
+      const wrapper = mount(Discovery, {
+        global: {
+          plugins: [createTestingPinia({ createSpy: vi.fn }), i18n],
+        },
+      });
+      await wrapper.vm.$nextTick();
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call fetchEcommerceApps', async () => {
+      const spy = vi.spyOn(Discovery.methods, 'fetchEcommerceApps');
+      const wrapper = mount(Discovery, {
+        global: {
+          plugins: [createTestingPinia({ createSpy: vi.fn }), i18n],
+        },
+      });
+      await wrapper.vm.$nextTick();
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call fetchFeatured', async () => {
+      const spy = vi.spyOn(Discovery.methods, 'fetchFeatured');
+      const wrapper = mount(Discovery, {
+        global: {
+          plugins: [createTestingPinia({ createSpy: vi.fn }), i18n],
+        },
+      });
+      await wrapper.vm.$nextTick();
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call callManuallyCreateApp if createAppCode isn`t null', async () => {
+      const spy = vi.spyOn(Discovery.methods, 'callManuallyCreateApp');
+      const wrapper = mount(Discovery, {
+        global: {
+          plugins: [createTestingPinia({ createSpy: vi.fn }), i18n],
+          mocks: {
+            $route: {
+              query: { create_app: 'tg' },
+            },
+          },
+        },
+      });
+      await wrapper.vm.$nextTick();
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
   });
 });
