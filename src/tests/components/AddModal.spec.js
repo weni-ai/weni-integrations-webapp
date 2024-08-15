d91bebb53d1cf8fc45f06694713bff3afa6de173
acnormun <anaclara.noronha@weni.ai>
2024-08-15 16:29:56 -0300

    chore: add AddModal tests

---
+++ src/tests/components/AddModal.spec.js
@@ -0,0 +1,27 @@
+import { mount } from '@vue/test-utils';
+import AddModal from '../../components/AddModal/index.vue';
+import { describe, expect, it, vi, beforeEach } from 'vitest';
+import i18n from '@/utils/plugins/i18n';
+describe('AddModal', () => {
+  let wrapper;
+  beforeEach(async () => {
+    wrapper = mount(AddModal, {
+      global: {
+        plugins: [i18n],
+      },
+    });
+    await wrapper.vm.$nextTick();
+  });
+
+  it('matches snapshot', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  describe('methods', () => {
+    it('should change showModal', async () => {
+      expect(wrapper.vm.showAddModal).toBe(false);
+      wrapper.vm.toggleModal();
+      expect(wrapper.vm.showAddModal).toBe(true);
+    });
+  });
+});
