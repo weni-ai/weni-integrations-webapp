42b3636ca3ec5bb9d37ff2bdb955acb2129010e4
acnormun <anaclara.noronha@weni.ai>
2024-11-12 15:57:10 -0300

    fix: callback route

--- src/router/index.js
+++ src/router/index.js
@@ -128,8 +128,8 @@ const router = createRouter({
       name: 'Google Callback',
       component: null,
       beforeEnter: (to, from, next) => {
-        const { code } = to.props;
-        console.log('entrou na rota', JSON.stringify(to.props));
+        const { code } = to.params;
+        console.log('entrou na rota', JSON.stringify(to.params));
         email_store().login({ code });
         if (to.query.next) {
           next(to.query.next);
