3eef51679849fd55e73c8ca18efb130505bc3dd1
sheilagomes <sheila.gomes@vtex.com>
2026-05-21 14:55:03 -0300

    Fix EN, ES and PT and add RO translations

--- src/utils/plugins/i18n.js
+++ src/utils/plugins/i18n.js
@@ -3,11 +3,12 @@ import * as VueI18n from 'vue-i18n';
 import en from '../../locales/en.json';
 import pt_br from '../../locales/pt_br.json';
 import es_es from '../../locales/es_es.json';
-
+import ro_ro from '../../locales/ro_ro.json';
 const languages = {
   'en-us': en,
   'pt-br': pt_br,
   es: es_es,
+  'ro-ro': ro_ro,
 };
 
 const messages = Object.assign(languages);
