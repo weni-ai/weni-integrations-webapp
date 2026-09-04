643ce65ec0155377888c0872772e70e48bdb063a
Paulo Bernardo <paulo.bernardo@vtex.com>
2026-09-04 13:35:14 -0300

    Refactor ConfigModal and related components to remove confirmation modal logic, simplifying the closeModal functionality. Update tests to reflect changes in confirmation handling and ensure proper state management. Clean up localization files by removing unused confirmation strings.

--- src/components/config/ConfigModal.vue
+++ src/components/config/ConfigModal.vue
@@ -27,47 +27,10 @@
             :app="currentApp"
             :isConfigured="isConfigured"
             @closeModal="closeModal"
-            @setConfirmation="setConfirmation"
           />
         </div>
       </UnnnicDrawerContent>
     </UnnnicDrawerNext>
-
-    <UnnnicDialog
-      ref="unnnic-confirmation-modal"
-      class="config-confirmation-dialog"
-      :open="showConfirmationModal"
-      @update:open="handleConfirmationOpenUpdate"
-    >
-      <UnnnicDialogContent size="medium">
-        <UnnnicDialogHeader type="attention">
-          <UnnnicDialogTitle>
-            {{ $t('apps.config.confirmation.title') }}
-          </UnnnicDialogTitle>
-        </UnnnicDialogHeader>
-
-        <section
-          class="config-confirmation-dialog__description"
-          v-html="$t('apps.config.confirmation.description')"
-        />
-
-        <UnnnicDialogFooter>
-          <UnnnicDialogClose>
-            <UnnnicButton
-              ref="unnnic-remove-modal-close-button"
-              type="tertiary"
-              :text="$t('apps.config.confirmation.goBackToConfig')"
-            />
-          </UnnnicDialogClose>
-          <UnnnicButton
-            ref="unnnic-remove-modal-navigate-button"
-            type="primary"
-            :text="$t('general.confirm')"
-            @click="confirmClose()"
-          />
-        </UnnnicDialogFooter>
-      </UnnnicDialogContent>
-    </UnnnicDialog>
   </div>
 </template>
 
@@ -108,8 +71,6 @@ export default {
       type: '',
       currentApp: {},
       isConfigured: false,
-      showConfirmationModal: false,
-      needConfirmation: false,
       componentMapping: markRaw({
         wwc: wwcConfig,
         tg: telegramConfig,
@@ -135,10 +96,6 @@ export default {
       this.closeModal();
     },
     closeModal() {
-      if (this.needConfirmation) {
-        this.showConfirmationModal = true;
-        return;
-      }
       this.show = false;
       this.$emit('close');
     },
@@ -147,23 +104,6 @@ export default {
       this.currentApp = app;
       this.isConfigured = isConfigured;
       this.show = true;
-    },
-    setConfirmation(value) {
-      this.needConfirmation = value;
-    },
-    handleConfirmationOpenUpdate(open) {
-      if (!open) {
-        this.showConfirmationModal = false;
-      }
-    },
-    toggleConfirmationModal() {
-      this.showConfirmationModal = !this.showConfirmationModal;
-    },
-    confirmClose() {
-      this.needConfirmation = false;
-      this.showConfirmationModal = false;
-      this.show = false;
-      this.$emit('close');
     },
   },
   computed: {
@@ -219,13 +159,6 @@ export default {
 
   &__component {
     height: 100%;
-  }
-}
-
-.config-confirmation-dialog {
-  &__description {
-    padding: $unnnic-space-4;
-    color: $unnnic-color-fg-base;
   }
 }
 </style>
